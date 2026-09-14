c98956676034d795c11dd665221c7d29be7f452c
Suraj Prajapati <5uraj99199@gmail.com>
2026-09-14 23:38:12 +0530

    Time: 1 ms (99.18%) | Memory: 42.8 MB (8.79%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,33 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        // For special cases when x is 0 or 1, return x.
-        if (x == 0 || x == 1)
-            return x;
-
-        // Initialize the search range for the square root.
-        int start = 1;
-        int end = x;
-        int mid = -1;
-
-        // Perform binary search to find the square root of x.
-        while (start <= end) {
-            // Calculate the middle point using "start + (end - start) / 2" to avoid integer overflow.
-            mid = start + (end - start) / 2;
-
-            // If the square of the middle value is greater than x, move the "end" to the left (mid - 1).
-            if ((long) mid * mid > (long) x)
-                end = mid - 1;
-            else if (mid * mid == x)
-                // If the square of the middle value is equal to x, we found the square root.
-                return mid;
-            else
-                // If the square of the middle value is less than x, move the "start" to the right (mid + 1).
-                start = mid + 1;
+        int low = 0, high = x, ans = -1;
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long midsq = mid * mid;
+            if (midsq == x)
+                return (int)mid;
+            else if (midsq > x)
+                high = (int)mid - 1;
+            else {
+                ans = (int)mid;
+                low = (int)mid + 1;
+            }
         }
-
-        // The loop ends when "start" becomes greater than "end", and "end" is the integer value of the square root.
-        // However, since we might have been using integer division in the calculations,
-        // we round down the value of "end" to the nearest integer to get the correct square root.
-        return Math.round(end);
+        return ans;
     }
 }
